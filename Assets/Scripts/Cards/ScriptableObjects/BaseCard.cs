8b2237aed299eaef105d6f4a882ce2e1f891a46b
Yasaman <ya7ya7ya2@yahoo.com>
2026-02-24 15:50:10 +0100

    add newsletter option

--- Assets/Scripts/Cards/ScriptableObjects/BaseCard.cs
+++ Assets/Scripts/Cards/ScriptableObjects/BaseCard.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Localization;
-using static UnityEngine.GraphicsBuffer;
 
 namespace Deviloop
 {
